6d08d0025fb4e9621ff84918817f56d023e61f46
mark <boychuk.m.n@gmail.com>
2021-03-21 20:49:40 +0200

    WIP

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,10 +6,8 @@
       </ion-toolbar>
     </ion-header>
 
-    <!--    <ion- -->
-
-    <ion-content :fullscreen="true">
-      <div id="container">
+    <ion-content :fullscreen="true" v-if="service">
+      <div class="container">
         <strong>Is Keyless working?</strong>
 
         <div>
@@ -36,6 +34,11 @@
           <span style="color: red" v-else>No</span>
         </div>
 
+        <div style="">
+          <ion-label>Dry run</ion-label>
+          <ion-toggle slot="start" name="Dry run" :checked="dryRun" @onIonChange="dryRun = !dryRun"/>
+        </div>
+
         <div>
           RSSI: {{ beaconState?.rssi }}<br>
           Accuracy: {{ beaconState?.accuracy }}<br>
@@ -43,7 +46,8 @@
         </div>
 
         <div>
-          Last request: {{ lastRequest }}
+          Last request: {{ lastRequest }} <br>
+          Latency: {{ latency }}ms
         </div>
 
         <div style="margin-top: 2rem" v-if="debug">
@@ -52,17 +56,27 @@
         </div>
       </div>
     </ion-content>
+    <ion-content v-else>
+      <div class="container">
+        <strong>BLE not found 😢</strong>
+      </div>
+    </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import { IonContent, IonHeader, IonLabel, IonPage, IonTitle, IonToggle, IonToolbar } from '@ionic/vue';
 import { defineComponent } from 'vue';
 import { IBeacon, IBeaconPluginResult } from "@ionic-native/ibeacon";
 import { filter, tap } from 'rxjs/operators';
+import axios from 'axios';
+import moment from 'moment';
 
+const REQUEST_INTERVAL = 2000;
 const BEACON_ID = 'c29ce823-e67a-4e71-bff2-abaa32e77a98';
 const BEACON_NAME = 'door';
+
+const wait = (time: number) => new Promise((r) => setTimeout(r, time))
 
 const filterRegion = filter((data: IBeaconPluginResult) => {
   return data.region.identifier === BEACON_NAME
@@ -70,7 +84,7 @@ const filterRegion = filter((data: IBeaconPluginResult) => {
 
 const log = (message: string) => tap((data: IBeaconPluginResult) => {
   console.log(message);
-  console.debug(data);
+  // console.debug(data);
 });
 
 export default defineComponent({
@@ -80,51 +94,51 @@ export default defineComponent({
     IonHeader,
     IonPage,
     IonTitle,
-    IonToolbar
+    IonToolbar,
+    IonToggle,
+    IonLabel,
   },
   data() {
     return {
       region:  IBeacon.BeaconRegion(BEACON_NAME, BEACON_ID),
       service: IBeacon.Delegate(),
 
+      debug: '',
+
       beaconState: {},
+      dryRun:      true,
 
       isMonitoring: false,
       isRanging:    false,
       inRegion:     false,
       isNear:       false,
+      latency:      0,
       lastRequest:  '-',
-      debug:        ''
+
+      keyTimer: 0,
     }
   },
   methods:    {
     async prepare() {
-      // this.service.didEnterRegion()
-      //     .pipe(filterRegion)
-      //     .subscribe(() => console.log('✅ didEnterRegion'))
-      //
-      // this.service.didExitRegion()
-      //     .pipe(filterRegion)
-      //     .subscribe(() => console.log('✅ didExitRegion'))
+      this.service.monitoringDidFailForRegionWithError().pipe(
+          log('🚨 Monitoring failed')
+      ).subscribe(() => this.isMonitoring = false);
 
-      this.service.monitoringDidFailForRegionWithError()
-          .pipe(log('🚨 Monitoring failed'))
-          .subscribe(() => this.isMonitoring = false);
-
-      this.service.didStartMonitoringForRegion()
-          .pipe(filterRegion)
-          .pipe(log('✅ Started monitoring for iBeacon'))
+      this.service.didStartMonitoringForRegion().pipe(
+          filterRegion,
+          log('✅ Started monitoring for iBeacon')
+      )
           .subscribe(() => this.isMonitoring = true);
 
-      this.service.didDetermineStateForRegion()
-          .pipe(filterRegion)
-          .pipe(log('✅ Region state changed'))
-          .subscribe(data => this.inRegion = data.state === 'CLRegionStateInside');
+      this.service.didDetermineStateForRegion().pipe(
+          filterRegion,
+          log('✅ Region state changed')
+      ).subscribe(data => this.inRegion = data.state === 'CLRegionStateInside');
 
-      this.service.didRangeBeaconsInRegion()
-          .pipe(filterRegion)
-          .pipe(log('✅ iBeacons detected'))
-          .subscribe(data => this.beaconState = data.beacons[0]);
+      this.service.didRangeBeaconsInRegion().pipe(
+          filterRegion,
+          log('✅ iBeacons detected')
+      ).subscribe(data => this.beaconState = data.beacons[0]);
     },
 
     async start() {
@@ -157,15 +171,60 @@ export default defineComponent({
       if (!this.inRegion && this.isRanging) {
         return this.stopRanging();
       }
+    },
+
+    async sendRequest() {
+      const url = 'https://admin.nerdzlab.com/api/door-bot/link/W8bCIZIIpnLZ7ktjmyykHc8O5y55iSvTuxWZ0U8GaHRJwbDJkVGhzAjuW8UFGQwnWkPI85iTpLWvaX9k28ImQffMHwsAKEmj9Rrw'
+      //const url = 'https://home.mar4ik.com/api/webhook/sbghjklsdfghvibehotew78vu32b985yfvb389r27b4fy1289vcbhjrwgfhkldbxLJZO'
+
+      const t1 = +new Date();
+
+      if (this.dryRun) {
+        await wait(200);
+      } else {
+        await axios.post(url);
+      }
+
+      const t2 = +new Date();
+
+      console.log('⏳ Request send');
+
+      this.latency = t2 - t1;
+      this.lastRequest = moment().format('hh:mm:ss')
+    },
+
+    startRequestTimer() {
+      this.keyTimer = setInterval(() => this.sendRequest(), REQUEST_INTERVAL);
+    },
+
+    stopRequestTimer() {
+      clearInterval(this.keyTimer);
+      this.keyTimer = 0;
+    },
+
+    toggleKey() {
+      if (this.keyTimer) {
+        return this.stopRequestTimer();
+      }
+
+      this.startRequestTimer()
     }
   },
   mounted() {
+    if (!this.service) {
+      return;
+    }
+
     this.prepare()
     this.start()
   },
   watch:      {
     inRegion() {
       this.toggleRanging();
+    },
+
+    isNear() {
+      this.toggleKey();
     },
 
     beaconState(newValue) {
@@ -177,7 +236,7 @@ export default defineComponent({
 </script>
 
 <style scoped>
-#container {
+.container {
   text-align: center;
 
   position: absolute;
@@ -187,12 +246,12 @@ export default defineComponent({
   transform: translateY(-50%);
 }
 
-#container strong {
+.container strong {
   font-size: 1.25rem;
   line-height: 1.5rem;
 }
 
-#container div {
+.container div {
   /*text-align: left;*/
   margin-top: 1rem
 }
